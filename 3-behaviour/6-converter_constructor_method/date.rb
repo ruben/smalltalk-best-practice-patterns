50dbd5d454e1646a709dfab5d1489cdca43e2b7e
Rubén Gil <rubengil22@gmail.com>
2013-12-09 16:19:26 +0100

    Converter construction method

---
+++ 3-behaviour/6-converter_constructor_method/date.rb
@@ -0,0 +1,8 @@
+# How do you represent the conversion of an object to another with different protocol?
+#
+# Make a Constructor Method that takes the object to be converted as an argument.
+#
+class Date
+  def initialize(string)
+  end
+end
