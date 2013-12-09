4be701afc228bcb1ccf2c1c9293a10407b0349c1
Rubén Gil <rubengil22@gmail.com>
2013-12-09 15:40:58 +0100

    Shortcut constructor method

---
+++ 3-behaviour/4-shortcut_constructor_method/1-point.rb
@@ -0,0 +1,15 @@
+# What is the external interface for creating a new object when a Constructor Method is too wordy?
+#
+# Represent object creation as a message to one of the arguments to the Constructor Method.
+# Add no more than three of these Shortcut Constructor Methods per system you develop.
+#
+class Point
+  def initialize(x, y)
+  end
+end
+
+class Number
+  def at(y)
+    return Point.new(self, y)
+  end
+end
