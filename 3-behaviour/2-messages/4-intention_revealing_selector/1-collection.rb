33e12937459754eceaf3ac4035b30083334295b0
Rubén Gil <rubengil22@gmail.com>
2013-12-11 14:48:52 +0100

    Intention Revealing Selector

---
+++ 3-behaviour/2-messages/4-intention_revealing_selector/1-collection.rb
@@ -0,0 +1,34 @@
+# What do you name a method?
+#
+# Name methods after what they accomplish
+
+# Naming methods after how they accomplish the task
+# Trick: Imagine a different implementation and see if you would give that method the same name.
+#
+class Array
+  def linear_search_for
+  end
+end
+
+class Set
+  def hashed_search_for
+  end
+end
+
+class BTree
+  def tree_search_for
+  end
+end
+
+# Naming method after the task they accomplish
+
+class Collection
+  def search_for
+  end
+end
+
+# Even more generic
+class Collection
+  def include
+  end
+end
