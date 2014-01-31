4fb7621c6b28267723261b959a924b47a2b18a27
Rubén Gil <rubengil22@gmail.com>
2014-01-31 12:44:18 +0100

    Added Common State

---
+++ 3-behaviour/3-state/1-common_state.rb
@@ -0,0 +1,9 @@
+# How do you represent state, different values for which exist in all instances of the class?
+#
+# Declare an instance variable in the class
+
+class CartesianPoint
+  def initialize(x, y)
+    @x, @y = x, y
+  end
+end
