10313ddc75d4aeb83a798c6f1b9043460e715184
Rubén Gil <rubengil22@gmail.com>
2014-02-03 12:04:46 +0100

    Explicit initialization

---
+++ 3-behaviour/3-state/2-explicit_initialization.rb
@@ -0,0 +1,15 @@
+# How do you initialize instance variables to their default value?
+#
+# Implement a method "initialize" that sets all the values explicitly.
+# Override the class message new to invoke it on all instances.
+
+class Timer
+  def default_millisecond_period
+    1000
+  end
+
+  def initialize
+    @period = default_millisecond_period
+    @count = 0
+  end
+end
