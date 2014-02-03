38f556cfb4c84b60a0c5c85cf7276ca39dd7f09a
Rubén Gil <rubengil22@gmail.com>
2014-02-03 12:31:42 +0100

    Default value method

---
+++ 3-behaviour/3-state/4-default_value.rb
@@ -0,0 +1,11 @@
+# How do you represent the default value of a variable?
+#
+# Create a method that returns the value.
+# Append default to the name of the variable as a name of the method.
+# If you are computing the value of an Caching Instance Variable call the method compute_ instead of default_
+
+class Book
+  def default_synopsis
+    ""
+  end
+end
