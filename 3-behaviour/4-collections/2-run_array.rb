3707bf8c57759fd517450e8b2f97adc6f14472b8
Rubén Gil <rubengil22@gmail.com>
2014-02-12 11:47:50 +0100

    RunArray

---
+++ 3-behaviour/4-collections/2-run_array.rb
@@ -0,0 +1,6 @@
+# How do you compact code on an OrderedCollection or Array where you have
+# the same element many times in a row?
+#
+# Use a RunArray to compress long runs of the same element.
+
+# Ruby does not implement RunArray
